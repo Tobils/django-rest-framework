77a43e4a03f30c93770ae2b02a3dd236f7cde282
Tobils <ade.suhada@mail.ugm.ac.id>
2022-11-04 22:11:12 +0700

    add serializer for post

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -3,12 +3,23 @@ from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 from rest_framework.decorators import api_view
 
-@api_view()
+@api_view(['GET','POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    print(movies.values())
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+      movies = Movie.objects.all()
+      print(movies.values())
+      serializer = MovieSerializer(movies, many=True)
+      return Response(serializer.data)
+    
+    if request.method == 'POST':
+      serializer = MovieSerializer(data=request.data)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+      else:
+        return Response(serializer.errors)
+
+
 
 @api_view()
 def movie_details(request, id):
