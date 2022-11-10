2b7f23fa4b62ab890b54cd924990d7dd12c7f391
Tobils <ade.suhada@mail.ugm.ac.id>
2022-11-10 09:34:19 +0700

    add overwrite queryset

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from watchlist_app.api.views import WatchListAV, WatchDetail, StreamPlatformAV, StreamPlatformDetails, ReviewList, ReviewDetail
+from watchlist_app.api.views import WatchListAV, WatchDetail, StreamPlatformAV, StreamPlatformDetails, ReviewList, ReviewDetail, ReviewCreate
 
 urlpatterns = [
     path('', WatchListAV.as_view(), name='watch-list'),
@@ -8,10 +8,11 @@ urlpatterns = [
     path('stream', StreamPlatformAV.as_view(), name='streamplatform-list'),
     path('stream/<int:id>', StreamPlatformDetails.as_view(), name='streamplatform-detail'),
 
-    # path('stream/<int:id>/review', StreamPlatformDetails.as_view(), name='streamplatform-detail'),
-    # path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    path('stream/<int:pk>/review-create', ReviewCreate.as_view(), name='review-create'),
+    path('stream/<int:pk>/review', ReviewList.as_view(), name='review-list'),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 
 
-    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
-    path('review', ReviewList.as_view(), name='review-list')
+    # path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    # path('review', ReviewList.as_view(), name='review-list')
 ]
