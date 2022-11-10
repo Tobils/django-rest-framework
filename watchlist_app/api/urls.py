449a27e3cad870a5b8f10f1f2e44fba89426fbe6
Tobils <ade.suhada@mail.ugm.ac.id>
2022-11-10 09:02:33 +0700

    belajar generic api view and mixin and concrete view clases

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from watchlist_app.api.views import WatchListAV, WatchDetail, StreamPlatformAV, StreamPlatformDetails
+from watchlist_app.api.views import WatchListAV, WatchDetail, StreamPlatformAV, StreamPlatformDetails, ReviewList, ReviewDetail
 
 urlpatterns = [
     path('', WatchListAV.as_view(), name='watch-list'),
     path('<int:id>', WatchDetail.as_view(), name='watch-details'),
     path('stream', StreamPlatformAV.as_view(), name='streamplatform-list'),
-    path('stream/<int:id>', StreamPlatformDetails.as_view(), name='streamplatform-detail')
+    path('stream/<int:id>', StreamPlatformDetails.as_view(), name='streamplatform-detail'),
+
+    # path('stream/<int:id>/review', StreamPlatformDetails.as_view(), name='streamplatform-detail'),
+    # path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+
+
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    path('review', ReviewList.as_view(), name='review-list')
 ]
