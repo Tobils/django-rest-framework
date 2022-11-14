f2d2eb986794fb712e3e13d30b88d38d0cc8b129
Tobils <ade.suhada@mail.ugm.ac.id>
2022-11-14 17:56:32 +0700

    add custom review calcultaion

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -28,6 +28,13 @@ class ReviewCreate(generics.CreateAPIView):
     if review_user.exists():
       raise ValidationErr("You have already give the review !")
     
+    if movie.number_rating == 0:
+      movie.avg_rating = serializer.validated_data['rating']
+    else: 
+      movie.avg_rating = (movie.avg_rating + serializer.validated_data['rating'])/2
+    
+    movie.number_rating = movie.number_rating + 1
+    movie.save()
     return serializer.save(watchlist=movie, review_user=user)
 
 
--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -15,9 +15,11 @@ class WatchList(models.Model):
   title = models.CharField(max_length=50)
   storyline = models.CharField(max_length=50)
   active = models.BooleanField(default=True)
+  avg_rating = models.FloatField(default=0)
+  number_rating = models.IntegerField(default=0)
   created = models.DateTimeField(auto_now_add=True)
   platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name="watchlist")
-
+ 
   def __str__(self):
       return self.title
   
